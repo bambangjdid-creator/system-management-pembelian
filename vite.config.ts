155494405ccc097c791b324eaa14e0cee7a5fe0c
Doumbozo <bambang.jdid@gmail.com>
2026-06-18 19:27:54 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({mode}) => {
   return {
     plugins: [react(), tailwindcss()],
     define: {
+      base: '/system-management-pembelian/',
+      plugins: [react(), tailwindcss()],
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
